a4bdd821540451ee314871320617139acc08ffb4
Nishant Sharma <sudoaccessdenied@gmail.com>
2026-04-14 22:51:26 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public const string Delimeter = "=>";
+    public string Encode(IList<string> strs) {
+        if(strs.Count ==0) return null;
+
+        // return string.Join(Delimeter,strs);
+        var sb = new StringBuilder();
+        var first = false;
+        foreach(var str in strs){
+
+            if(first){
+                sb.Append(Delimeter);
+            }
+            first = true;
+            sb.Append(str);
+        }
+        return sb.ToString();
+    }
+
+    public List<string> Decode(string s) {
+        if(s == null) return new List<string>();
+        var strs = s.Split(Delimeter);
+        return new List<string>(strs);
+   }
+}
